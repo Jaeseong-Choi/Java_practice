334f4556c67fccd7194c6adf71bd15f45d624564
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-03-30 16:48:38 +0900

    Title: 흰검 칠하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260330/흰검 칠하기/painting-white-black.java
+++ 260330/흰검 칠하기/painting-white-black.java
@@ -1,62 +1,55 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         
-        int[] arr = new int [1_000_001];
-        String[] color = new String[1_000_001];
+        // 충분한 크기의 배열 선언 (좌표 -500,000 ~ 500,000)
+        int offset = 500_000;
+        int[] cntW = new int[1_000_001];
+        int[] cntB = new int[1_000_001];
+        int[] color = new int[1_000_001]; // 0:무색, 1:흰색, 2:검은색, 3:회색
         
-        int idx = 0; 
+        int cur = 0; // 로봇의 현재 위치 (타일 번호)
         
         for (int i = 0; i < N; i++) {
             int x = sc.nextInt();
             char dir = sc.next().charAt(0);
             
             if (dir == 'R') {
-            	 for(int j = idx ; j < idx + x ; j++) {
-            		 if (color[j+500_000] == null || color[j+500_000].equals("White") ) {
-            			 color[j+500_000] = "Black";
-            			 arr[j+500_000]++;
-            			 if(arr[j+500_000] >= 4) color[j+500_000] = "Grey";
-            		 }
-             		
-
-            	 }
-            	 idx += x;
-            	 
-        	} else {
-        		for(int j = idx - 1; j >=  idx - x; j--) {
-        			if (color[j+500_000] == null || color[j+500_000].equals("Black") ) {
-        				 color[j+500_000] = "White";
-	           			 arr[j+500_000]++;
-	           			 if(arr[j+500_000] >= 4) color[j+500_000] = "Grey";
-  			 
-           		 }
-             }
-        		idx -= x;
+                // 현재 위치 포함 오른쪽으로 x칸: [cur, cur + x - 1]
+                for (int j = cur; j < cur + x; j++) {
+                    int pos = j + offset;
+                    if (color[pos] == 3) continue; // 이미 회색이면 무시
+                    
+                    color[pos] = 2; // 검은색으로 변경
+                    cntB[pos]++;
+                    if (cntW[pos] >= 2 && cntB[pos] >= 2) color[pos] = 3;
+                }
+                // 마지막으로 칠한 타일 위치에 정지
+                cur = cur + x - 1;
+            } else {
+                // 현재 위치 포함 왼쪽으로 x칸: [cur - x + 1, cur]
+                for (int j = cur; j > cur - x; j--) {
+                    int pos = j + offset;
+                    if (color[pos] == 3) continue;
+                    
+                    color[pos] = 1; // 흰색으로 변경
+                    cntW[pos]++;
+                    if (cntW[pos] >= 2 && cntB[pos] >= 2) color[pos] = 3;
+                }
+                // 마지막으로 칠한 타일 위치에 정지
+                cur = cur - x + 1;
+            }
         }
-            
-       } 
-       
-        int black = 0;
-        int white = 0;
-        int grey = 0;
         
-       for(int i = 0; i < arr.length; i++) {
-    	   if (color[i] == null) continue;
-    	   if (arr[i] > 0) {
-    		   if (color[i].equals("Black")) {
-    			   black++;
-    		   } else if (color[i].equals("White")) {
-    			   white++;
-    		   } else {
-    			   grey++;
-    		   }
-    	   }
-       }
-       
-       System.out.println(white + " " + black + " " + grey);
-
+        int w = 0, b = 0, g = 0;
+        for (int i = 0; i < color.length; i++) {
+            if (color[i] == 1) w++;
+            else if (color[i] == 2) b++;
+            else if (color[i] == 3) g++;
+        }
+        System.out.println(w + " " + b + " " + g);
     }
 }
