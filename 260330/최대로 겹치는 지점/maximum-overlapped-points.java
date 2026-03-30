bcf7f672436172709322298faaf0aa9e77eefcc3
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-03-30 14:50:02 +0900

    Title: 최대로 겹치는 지점, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260330/최대로 겹치는 지점/maximum-overlapped-points.java
+++ 260330/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int[] arr = new int[100];
+        int[] arr = new int[101];
         
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < a; i++) {
