a4ccb4d0ff847df425cc167acc59fb98489d5165
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-03-30 14:38:30 +0900

    Title: 블럭쌓는 명령2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260330/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        
+        int[] arr = new int[N+1];
+        
+        for (int i = 0; i < K; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            
+            for(int j = A; j <= B; j++) {
+            	arr[j]++;
+            }
+        }
+        
+        
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i < N; i++) {
+        	if (arr[i] > max) max = arr[i]; 
+        }
+
+        System.out.println(max);
+        
+    }
+}
