38f4915cb037ee050873d6d8ebba47b0720071c0
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-03-31 00:57:51 +0900

    Title: 연속되는 수 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260331/연속되는 수 3/continuous-number3.java
+++ 260331/연속되는 수 3/continuous-number3.java
@@ -16,16 +16,12 @@ public class Main {
         int max = 1;
         
         for (int i = 0; i < arr.length - 1; i++) {
-        	
-        	 if (arr[i] > 0 && arr[i+1] > 0) {
-        		 cnt++;
-        		 if (cnt > max) max = cnt;
-        	 } else {
+     
+        	 if (arr[i] * arr[i+1] < 0) cnt = 1;
+        	 else {
         		 cnt++;
         		 if (cnt > max) max = cnt;
         	 }
-        	 
-        	 if (arr[i] * arr[i+1] < 0) cnt = 1;
         }
         
         System.out.println(max);
