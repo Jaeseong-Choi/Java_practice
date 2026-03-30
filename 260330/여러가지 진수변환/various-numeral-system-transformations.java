d817ca13198ab7e961c2bbc09fe3371c1320a1d1
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-03-30 14:37:43 +0900

    Title: 여러가지 진수변환, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260330/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int[] arr = new int[1000];
+        int cnt = 0;
+        
+        while(true) {
+        	if (n <= 0) {
+        		break;
+        	}
+        	
+        	arr[cnt++] = n % b;
+        	n /= b;
+        	
+        	
+        }
+        
+        for (int i = cnt - 1; i >= 0; i--) {
+        	System.out.print(arr[i] + "");
+        }
+        
+    }
+}
