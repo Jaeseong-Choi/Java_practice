92093d7cbedcaf9d8f085d3e5369f90683e1b498
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-03-31 11:37:47 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260331/T를 초과하는 연속 부분 수열/subsequence-above-t.java
+++ 260331/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -10,14 +10,14 @@ public class Main {
         	arr[i] = sc.nextInt();
         }
         
-        int cnt = 1; 
-        int max = 1; 
+        int cnt = 0; 
+        int max = 0; 
         
-        for(int i = 0; i < n-1; i++) {
-        	if (arr[i] > t && arr[i] < arr[i+1] ) {
+        for(int i = 0; i < n; i++) {
+        	if (arr[i] > t) {
         		cnt++; 
         		if (cnt > max) max = cnt;
-        	} else cnt = 1;
+        	} else cnt = 0;
         }
         
         System.out.println(max);
