0aa100aaaaf296214db0eb79ef9480fdd5576971
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-04-08 12:13:09 +0900

    Title: 일렬로 서있는 소 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+        	arr[i] = sc.nextInt();
+        }
+        int cnt = 0; 
+        
+        for (int i = 0; i < n; i++) {
+        	for (int j = i+1; j < n; j++) {
+        		if (arr[i] > arr[j]) continue; 
+        		for (int k = j+1; k < n; k++) {
+        			if (arr[j] > arr[k]) continue; 
+        			if (arr[i] <= arr[j] && arr[j] <= arr[k]) cnt++;
+        		}
+        	}
+        }
+        
+        System.out.println(cnt);
+        
+
+    }
+}
+
