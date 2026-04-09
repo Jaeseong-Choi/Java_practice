73aa58e309884a3b72f95b9762839fb05ef4896d
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-04-09 15:32:08 +0900

    Title: 인접하지 않은 2개의 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260409/인접하지 않은 2개의 수/two-non-adjacent-numbers.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+        	arr[i] = sc.nextInt();
+        }
+        
+        int max = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < n; i++) {	
+        	for (int j = i+2; j < n; j++) {
+        		
+        			int sum = arr[i] + arr[j]; 
+        			if (sum > max) max = sum;
+        	}
+        }
+        
+        System.out.println(max);
+        
+    }//main
+}//MAIN
+
