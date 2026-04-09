48e5a9e4ff86f51e456425ad2e50b20755181ea1
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-04-09 11:00:26 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260409/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] x = new int[n];
+        int[] y = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+        	x[i] = sc.nextInt();
+        	y[i] = sc.nextInt();
+        }
+        
+        int min = Integer.MAX_VALUE;
+        
+        int bx = 0;
+        int by = 0;
+        
+        for (int skip = 1; skip < n - 1; skip++) {
+        	int sum = 0; 
+        	for (int i = 0; i < n; i++) {
+        		if (skip == i) continue; 
+        		
+        		sum += Math.abs(x[bx] - x[i]) + Math.abs(y[by] - y[i]);
+        		bx = i;
+        		by = i;
+        	}
+        	
+        	
+        	min = Math.min(sum, min);
+        }
+        
+        System.out.println(min);
+
+    }//main
+}//MAIN
+
