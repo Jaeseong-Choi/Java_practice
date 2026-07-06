00b37bc61bf45c79eaf7481d61992710cb546785
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-07-06 16:05:12 +0900

    [Passed] 달에서 무게 구하기 (103ms, 9MB)

---
+++ 260706/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 13; 
+        double b = 0.165000;
+
+        System.out.printf("%d * %.6f = %.6f%n", a, b, a * b);
+    }
+}
