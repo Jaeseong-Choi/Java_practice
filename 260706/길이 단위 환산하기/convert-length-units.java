506d4b8c70350d2b516817c0cc8a314c86f98e07
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-07-06 17:02:06 +0900

    [Passed] 길이 단위 환산하기 (146ms, 11MB)

---
+++ 260706/길이 단위 환산하기/convert-length-units.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+
+        double ft = 30.48;
+
+        System.out.printf("%.1f", n*ft);
+    }
+}
