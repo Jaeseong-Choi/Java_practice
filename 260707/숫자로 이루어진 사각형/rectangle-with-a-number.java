c8f025d2d35e6c4709f7218306e9ac27ae9122ee
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-07-07 16:57:51 +0900

    [Passed] 숫자로 이루어진 사각형 (135ms, 11MB)

---
+++ 260707/숫자로 이루어진 사각형/rectangle-with-a-number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void rec(int n) {
+        int a = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a++ + " ");
+                if(a > 9) {
+                    a = 1;
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+
+        rec(n);
+    }
+}
