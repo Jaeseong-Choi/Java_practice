ac4538a31fe748740d76bd42cd620fd7d9395b89
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-07-07 13:00:51 +0900

    [Passed] 전화번호 바꾸기 (122ms, 11MB)

---
+++ 260707/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        //월 일 년  / 년 월 일 
+
+        String str = sc.next();
+        String[] strArr = str.split("-");
+
+        System.out.println(strArr[0] + "-" + strArr[2] + "-" + strArr[1]);
+        
+    }
+}
