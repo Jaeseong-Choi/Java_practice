3a57d616c944aa57ae35180d61f7e87bed293dd8
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-07-07 13:22:54 +0900

    [Passed] 2개의 정수를 서로 더하기 (139ms, 10MB)

---
+++ 260707/2개의 정수를 서로 더하기/add-two-integers-each-other.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        //b -> a ++, a -> b++
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        a += b;
+        b += a;
+
+        System.out.println(a + " " + b);
+    }
+}
