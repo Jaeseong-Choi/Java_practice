1567954c36aee4c4ea36a2d3f59385a76a34225a
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-07-07 13:32:38 +0900

    [Passed] 합과 차의 나눗셈 (147ms, 11MB)

---
+++ 260707/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        double c = (a+b) / (a-b);
+
+        System.out.printf("%.2f", c);
+    }
+}
