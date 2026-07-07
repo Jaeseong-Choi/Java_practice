8d5533847c7f7803e8f26f55ae304dcf1ac1d50c
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-07-07 14:25:00 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (89ms, 9MB)

---
+++ 260707/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void stars() {
+         for (int j = 0; j < 10; j++) {
+                System.out.print("*");
+            }
+    }
+    public static void main(String[] args) {
+        // Please write your code here.
+        for(int i = 0; i < 5; i++) {
+            stars();
+            System.out.println();
+        }
+    }
+}
