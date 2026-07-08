2c391c7b7b99a8abea19b884cb9fc148772f0b84
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-07-08 23:49:17 +0900

    [Passed] 함수를 이용한 소수 판별 (150ms, 10MB)

---
+++ 260708/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static boolean isPrime (int n) {
+        if (n == 1) return false;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+        
+
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Please write your code here.
+
+        int sum = 0;
+
+        for (int i = Math.min(a,b); i <= Math.max(a,b); i++) {
+            if(isPrime(i)) sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
