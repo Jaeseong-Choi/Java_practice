9dbd57009d70b69df5aae54d665cafa9717b8fb6
Jaeseong-Choi <jaeseong.choiiii@gmail.com>
2026-08-19 08:22:21 +0900

    [Passed] 특정 조건에 맞게 출력하기 (132ms, 10MB)

---
+++ 260819/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
+
+        for (int i = 0; i < 100; i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i] == 0) {
+                for (int j = 0; j < i; j++ ) {
+                    if(arr[j] % 2 == 1) {
+                        System.out.print((arr[j] + 3) + " ");
+                    } else {
+                        System.out.print((arr[j] / 2) + " ");
+                    }
+                }
+                break;
+            }
+        }
+
+    }
+}
